e0b509f28dc307829708275bcff0c63b52e17cec
Johan Heander <johan@timezynk.com>
2022-10-25 15:00:41 +0200

    Add import

--- src/android/GCMPlugin.java
+++ src/android/GCMPlugin.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import androidx.annotation.NonNull;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.messaging.FirebaseMessaging;
 import java.util.Map;
 import org.apache.cordova.*;
